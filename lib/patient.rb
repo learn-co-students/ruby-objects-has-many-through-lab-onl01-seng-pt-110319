f9bb754940a7f45d67c514cdf9ce667e6e612684
Jordan <jordanleelong5@gmail.com>
2019-12-16 13:32:46 -0800

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,30 @@
+class Patient
+attr_accessor :name
+@@all = []
+
+def initialize(name)
+    @name = name
+    @@all << self
+end
+
+def appointments
+  
+    Appointment.all.select { |a| a.patient == self}
+end
+
+
+
+def self.all
+    @@all
+end
+
+def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+end
+
+def doctors
+# binding.pry
+appointments.map {|a| a.doctor}
+end
+
+end
