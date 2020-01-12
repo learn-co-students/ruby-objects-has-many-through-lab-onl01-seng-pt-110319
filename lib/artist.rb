dfad7f5a5551568b526cbb77f2f37b3f90051ea6
arnie b <arnold.bwaila@gmail.com>
2020-01-12 16:01:49 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name, genre, self)
+  end
+  
+  def songs 
+    Songs.all.select {|s| s.artist == self }
+  end 
+  
+  def genres 
+    self.songs.collect { |s| s.genre}
+  end 
+  
+end
+    
